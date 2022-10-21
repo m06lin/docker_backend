d90531d5836577e5e71dc263bc1eeb29f019263f
Alex <alex.hsu@accuhit.net>
2022-10-21 14:04:57 +0800

    feat: remove kakfa, update docs

--- nginx/add_host.sh
+++ nginx/add_host.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 FILENAME=$1
+CONTAINER_NAME=${2:-php-fpm}
 cp ./conf.d/laravel.example ./conf.d/$FILENAME.conf
 sed -i '' "s/{xxxxx}/$FILENAME/g" ./conf.d/$FILENAME.conf
+sed -i '' "s/{CONTAINER_NAME}/$CONTAINER_NAME/g" ./conf.d/$FILENAME.conf
 
 echo "create $FILENAME.conf success"
